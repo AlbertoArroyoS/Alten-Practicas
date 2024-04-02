b898f61ce0ab7e721b71624de2db5847f35ca929
Alberto Arroyo <albertoarroyo@hotmail.es>
2024-04-02 15:30:55 +0200

    Inicio Proyecto Spring boot en eclipse

---
+++ Back/LibreriasBACKEND/src/main/java/com/alten/practica/LibreriasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.alten.practica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreriasBackendApplication.class, args);
+	}
+
+}
---
+++ Back/LibreriasBACKEND/src/test/java/com/alten/practica/LibreriasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alten.practica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreriasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
