ab8f9d172a9456d9482f12c28ff73f839674e268
Alberto Arroyo <albertoarroyo@hotmail.es>
2024-04-03 12:54:48 +0200

    inteface de libreria implJPA

--- Back/LibreriasBACKEND/src/main/java/com/alten/practica/modelo/persistencia/DaoLibreriaJPA.java
+++
@@ -1,5 +0,0 @@
-package com.alten.practica.modelo.persistencia;
-
-public class DaoLibreriaJPA {
-
-}
--- Back/LibreriasBACKEND/src/main/java/com/alten/practica/modelo/persistencia/interfaz/DaoLibreria.java
+++ Back/LibreriasBACKEND/src/main/java/com/alten/practica/modelo/persistencia/interfaz/DaoLibreria.java
@@ -1,5 +1,21 @@
 package com.alten.practica.modelo.persistencia.interfaz;
 
-public interface DaoLibreria {
+import java.util.List;
 
+import com.alten.practica.modelo.entidad.Libreria;
+
+public interface DaoLibreria {
+	
+	//CRUD
+	//Añadir Libreria
+	public int addLibreria(Libreria libreria);
+	//Borrar Libreria
+	public int deleteLibreria(int id);
+	//Actualizar Libreria
+	public int updateLibreria(Libreria libreria);
+	//Obtener Libreria por id
+	public Libreria getLibreria(int id);
+	//Obtener todas las librerias en una lista
+	public List<Libreria> getLibrerias();
+	
 }
