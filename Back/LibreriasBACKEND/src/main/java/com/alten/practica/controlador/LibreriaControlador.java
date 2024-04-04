13c609a7684b9c2144e41cb2aee1fdab4bd32d1e
Alberto Arroyo <albertoarroyo@hotmail.es>
2024-04-04 15:48:17 +0200

    solucion error del pom

--- Back/LibreriasBACKEND/src/main/java/com/alten/practica/controlador/LibreriaControlador.java
+++ Back/LibreriasBACKEND/src/main/java/com/alten/practica/controlador/LibreriaControlador.java
@@ -1,13 +1,33 @@
 package com.alten.practica.controlador;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.alten.practica.modelo.entidad.Libreria;
+import com.alten.practica.service.LibreriaService;
 
 @RestController
 @RequestMapping("/libreria")
 @CrossOrigin("*") //Permite que el FrontEnd se conecte a este controlador, de momento todos, luego se especificará
 public class LibreriaControlador {
+	
+	private LibreriaService libreriaService;
+
+	public LibreriaControlador(LibreriaService libreriaService) {
+		super();
+		this.libreriaService = libreriaService;
+	}
+	
+	
+	//@GetMapping para listar todos las librerias de la base de datos
+	@GetMapping("/findAll")
+	public List<Libreria> findAll () {
+		return this.libreriaService.findAll();
+	}
 	
 	
 
