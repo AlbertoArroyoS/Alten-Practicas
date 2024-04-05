eead76c57f64b11d4bf13c1c41960c8f6c3dbf9c
Alberto Arroyo <albertoarroyo@hotmail.es>
2024-04-05 16:14:35 +0200

    Clase constantes actualizacion

--- Back/LibreriasBACKEND/src/main/java/com/alten/practica/constantes/LibreriaConstant.java
+++ Back/LibreriasBACKEND/src/main/java/com/alten/practica/constantes/LibreriaConstant.java
@@ -4,9 +4,10 @@ public class LibreriaConstant {
 
 	//API version
 	public static final String API_VERSION = "/v1";
-	
-	//Recursos, path del sistema libreria
+
+	//contexto de la aplicacion
 	public static final String RESOURCE_GENERIC = API_VERSION + "/app-libreria";	
+	//Recursos, path del sistema libreria
 	public static final String RESOURCE_LIBRERIAS = "/librerias";	
 	public static final String RESOURCE_LIBRERIA = "/libreria";
 	public static final String RESOURCE_GENERIC_ID = "/{id}";
