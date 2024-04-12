ec12d2b5e00505f2d63245ce554ec56c03502b60
Alberto Arroyo <albertoarroyo@hotmail.es>
2024-04-12 11:34:41 +0200

    Paquete util para meter los metodos generales del proyecto y hago un metodo para la paginacionDTO

---
+++ Back/LibreriasBACKEND/src/main/java/com/alten/practica/util/LibreriaUtil.java
@@ -0,0 +1,43 @@
+package com.alten.practica.util;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Component;
+
+import com.alten.practica.dto.PageableDTO;
+
+//Clase donde se encuentran todos los metodos generales
+//como validar un email, un password, un nombre, un apellido, etc
+//Se puede usar en cualquier parte del proyecto
+
+@Component
+public class LibreriaUtil {
+	
+
+	/**
+	 * Metodo que devuelve un objeto Pageable Recibe un objeto PageableDTO, con el
+	 * orden, el campo, el numero de pagina y el tamaño de la pagina
+	 * 
+	 * @param pageableDTO
+	 * @return
+	 */
+	public Pageable getPageable(PageableDTO pageableDTO) {
+		Optional<Integer> sortOrder = pageableDTO.getOrder();
+		Optional<String> sortField = pageableDTO.getField();
+		Integer pageNumber = pageableDTO.getPageNumber();
+		Integer perPage = pageableDTO.getPageSize();
+
+		Pageable pageable;
+		if (sortOrder.isPresent() && sortField.isPresent()) {
+			Sort.Direction direction = sortOrder.get().equals(1) ? Sort.Direction.ASC : Sort.Direction.DESC;
+			pageable = PageRequest.of(pageNumber, perPage, Sort.by(direction, sortField.get()));
+		} else {
+			pageable = PageRequest.of(pageNumber, perPage, Sort.by(Sort.Direction.DESC, "id"));
+		}
+		return pageable;
+	}
+
+}
