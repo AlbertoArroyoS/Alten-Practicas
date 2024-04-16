464145ea373668612b4ae32d5f5228a84c0473ba
Alberto Arroyo <albertoarroyo@hotmail.es>
2024-04-16 11:07:26 +0200

    Inicio gestion de errores y excepciones en el servidor, EntityNotFoundException

---
+++ Back/LibreriasBACKEND/src/main/java/com/alten/practica/errorhandler/EntityNotFoundException.java
@@ -0,0 +1,33 @@
+package com.alten.practica.errorhandler;
+
+public class EntityNotFoundException extends RuntimeException{
+
+	public EntityNotFoundException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public EntityNotFoundException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public EntityNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public EntityNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public EntityNotFoundException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+}
