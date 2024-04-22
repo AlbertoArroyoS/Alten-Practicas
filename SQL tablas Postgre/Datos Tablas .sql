05ba265046dec5ee9bac25fbf037de0ea63661b9
Alberto Arroyo <albertoarroyo@hotmail.es>
2024-04-22 10:48:57 +0200

    SQL insertar datos actualizado a las nuevas tablas

--- SQL tablas Postgre/Datos Tablas .sql
+++ SQL tablas Postgre/Datos Tablas .sql
@@ -2,12 +2,14 @@
 
 --1. librerias ficticias
 
-INSERT INTO dbo.librerias (nivel_permiso, ciudad, direccion, nombre_dueno, nombre_libreria) 
+INSERT INTO dbo.librerias 
+    (nivel_permiso, ciudad, direccion, nombre_dueno, nombre_libreria, email) 
 VALUES 
-(1, 'Ciudad A', 'Calle 123', 'Juan Pérez', 'Librería ABC'),
-(2, 'Ciudad B', 'Avenida 456', 'María Gómez', 'Librería XYZ'),
-(3, 'Ciudad C', 'Plaza Principal', 'Carlos Martínez', 'Librería 123'),
-(1, 'Ciudad D', 'Calle Principal', 'Laura Rodríguez', 'Librería Test');
+    (1, 'Ciudad A', 'Calle 123', 'Juan Pérez', 'Librería ABC', 'emailabc@ejemplo.com'),
+    (2, 'Ciudad B', 'Avenida 456', 'María Gómez', 'Librería XYZ', 'emailxyz@ejemplo.com'),
+    (3, 'Ciudad C', 'Plaza Principal', 'Carlos Martínez', 'Librería 123', 'email123@ejemplo.com'),
+    (1, 'Ciudad D', 'Calle Principal', 'Laura Rodríguez', 'Librería Test', 'emailtest@ejemplo.com');
+
 
 
 --2. Insertar datos de autores 
@@ -34,31 +36,15 @@ BEGIN
     SELECT id_autor INTO stephenId FROM dbo.autores WHERE nombre = 'Stephen' AND apellidos = 'King';
     
     -- Insertar datos de libros con los IDs de autores
-    INSERT INTO dbo.libros (paginas, precio, autor_id, descripcion, editorial, genero, titulo)
+    INSERT INTO dbo.libros (paginas, id_autor, descripcion, editorial, genero, titulo)
     VALUES 
-    (300, 25.99, gabrielId, 'Una saga familiar que abarca varias generaciones de la familia Buendía en el ficticio pueblo de Macondo, describiendo la historia de su fundación y la decadencia de la familia.', 'Editorial Sudamericana', 'Ficción', 'Cien años de soledad'),
-    (400, 19.99, jkId, 'La historia sigue a Harry Potter, un joven mago, mientras descubre su verdadera identidad y lucha contra Lord Voldemort, el mago tenebroso que intentó asesinarlo cuando era un bebé.', 'Salani Editore', 'Fantasía', 'Harry Potter y la piedra filosofal'),
-    (250, 15.50, harperId, 'La novela explora los temas del racismo y la injusticia a través de la historia de Atticus Finch, un abogado que defiende a un hombre negro acusado de violar a una mujer blanca.', 'J.B. Lippincott & Co.', 'Novela', 'Matar a un ruiseñor'),
-    (350, 30.75, stephenId, 'Jack Torrance, un aspirante a escritor y alcohólico en recuperación, acepta un trabajo como cuidador de invierno del Hotel Overlook en las montañas de Colorado, lo que lleva a una serie de eventos terroríficos.', 'Doubleday', 'Terror', 'El resplandor');
+    (300, gabrielId, 'Una saga familiar que abarca varias generaciones de la familia Buendía en el ficticio pueblo de Macondo, describiendo la historia de su fundación y la decadencia de la familia.', 'Editorial Sudamericana', 'Ficción', 'Cien años de soledad'),
+    (400, jkId, 'La historia sigue a Harry Potter, un joven mago, mientras descubre su verdadera identidad y lucha contra Lord Voldemort, el mago tenebroso que intentó asesinarlo cuando era un bebé.', 'Salani Editore', 'Fantasía', 'Harry Potter y la piedra filosofal'),
+    (250, harperId, 'La novela explora los temas del racismo y la injusticia a través de la historia de Atticus Finch, un abogado que defiende a un hombre negro acusado de violar a una mujer blanca.', 'J.B. Lippincott & Co.', 'Novela', 'Matar a un ruiseñor'),
+    (350, stephenId, 'Jack Torrance, un aspirante a escritor y alcohólico en recuperación, acepta un trabajo como cuidador de invierno del Hotel Overlook en las montañas de Colorado, lo que lleva a una serie de eventos terroríficos.', 'Doubleday', 'Terror', 'El resplandor');
 END$$;
 
---De forma individual
 
-DO $$
-DECLARE
-    gabrielId INT;
-BEGIN
-    -- Obtener el ID del autor
-    SELECT id INTO gabrielId FROM dbo.autores WHERE nombre = 'Gabriel' AND apellidos = 'García Márquez';
-   
-    -- Insertar datos de libros con el ID del autor
-    INSERT INTO dbo.libros (paginas, precio, autor_id, descripcion, editorial, genero, titulo)
-    VALUES 
-    (300, 25.99, gabrielId, 'Una saga familiar que abarca varias generaciones de la familia Buendía en el ficticio pueblo de Macondo, describiendo la historia de su fundación y la decadencia de la familia.', 'Editorial Sudamericana', 'Ficción', 'Cien años de soledad');
-    
-    -- Guardar los cambios
-    COMMIT;
-END$$;
 
 
 
@@ -80,11 +66,12 @@ BEGIN
     SELECT id_libro INTO harryPotterId FROM dbo.libros WHERE titulo = 'Harry Potter y la piedra filosofal';
 
     -- Insertar registros en la tabla intermedia
-    INSERT INTO dbo.libreria_libro (libreria_id, libro_id, cantidad, precio)
+    INSERT INTO dbo.libreria_libro (id_libreria, id_libro, cantidad, edicion, precio, fecha_publicacion)
     VALUES 
-    (libreriaABCId, cienAniosDeSoledadId, 20, 30.99), -- Para la Librería ABC y "Cien años de soledad"
-    (libreriaXYZId, harryPotterId, 15, 25.99); -- Para la Librería XYZ y "Harry Potter y la piedra filosofal"
+        (libreriaABCId, cienAniosDeSoledadId, 20, 1, 30.99, '2023-01-01'), -- Para la Librería ABC y "Cien años de soledad"
+        (libreriaXYZId, harryPotterId, 15, 2 , 25.99, '2023-01-02'); -- Para la Librería XYZ y "Harry Potter y la piedra filosofal"
 END$$;
+
 
 
 
