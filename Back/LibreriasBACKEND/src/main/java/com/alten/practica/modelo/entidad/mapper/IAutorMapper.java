4081b35e00f58e7ccf2009d13d384c83eb792208
Alberto Arroyo <albertoarroyo@hotmail.es>
2024-04-23 07:42:52 +0200

    Continuacion documentacion codigo

--- Back/LibreriasBACKEND/src/main/java/com/alten/practica/modelo/entidad/mapper/IAutorMapper.java
+++ Back/LibreriasBACKEND/src/main/java/com/alten/practica/modelo/entidad/mapper/IAutorMapper.java
@@ -1,7 +1,6 @@
 package com.alten.practica.modelo.entidad.mapper;
 
 import org.mapstruct.Builder;
-
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -9,6 +8,13 @@ import com.alten.practica.modelo.entidad.Autor;
 import com.alten.practica.modelo.entidad.dto.AutorDTO;
 import com.alten.practica.modelo.entidad.dto.request.AutorDTORequest;
 
+/*
+ * Interfaz que se encarga de realizar el mapeo de la entidad a DTO y viceversa
+ * 
+ * @Mapper: indica que es una interfaz de mapeo
+ * @Builder: indica que se va a utilizar el patron builder para crear la entidad
+ * @Mapping: indica el mapeo de los atributos de la entidad a los atributos del DTO
+ */
 @Mapper (builder = @Builder(disableBuilder = true))
 public interface IAutorMapper {
 	
