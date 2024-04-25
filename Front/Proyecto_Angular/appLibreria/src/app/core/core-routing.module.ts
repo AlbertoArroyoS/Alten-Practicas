90066772319c474d20703529770ef0e61b453aec
Alberto Arroyo <albertoarroyo@hotmail.es>
2024-04-25 10:25:02 +0200

    si el path esta vacio que vaya a la autenticacion

--- Front/Proyecto_Angular/appLibreria/src/app/core/core-routing.module.ts
+++ Front/Proyecto_Angular/appLibreria/src/app/core/core-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //si el path esta vacio que se vaya al modulo autenticacion
+  {path : '', loadChildren: () => import('./../auth/auth.module').then(m => m.AuthModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
