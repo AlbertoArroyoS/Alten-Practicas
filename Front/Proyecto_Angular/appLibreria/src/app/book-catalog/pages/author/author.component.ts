29639b6516c13a33a076c20c82fb05b26ec602e8
Alberto Arroyo <albertoarroyo@hotmail.es>
2024-04-29 18:52:37 +0200

    busqueda y estilo tabla autores

--- Front/Proyecto_Angular/appLibreria/src/app/book-catalog/pages/author/author.component.ts
+++ Front/Proyecto_Angular/appLibreria/src/app/book-catalog/pages/author/author.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Component , OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-author',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./author.component.scss']
 })
 export class AuthorComponent {
+  public title!: string;
+
+
+  ngOnInit(): void {
+
+    this.title = 'Lista de autores';
+
+
+  }
 
 }
+
