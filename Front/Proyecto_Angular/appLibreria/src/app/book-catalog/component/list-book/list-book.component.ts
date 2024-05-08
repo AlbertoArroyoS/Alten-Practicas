6189d3086927e702ccf6c6e3d15be417cfb025cb
Alberto Arroyo <albertoarroyo@hotmail.es>
2024-05-08 19:22:45 +0200

    Acceso a Autores desde la lista de libros al añadir un libro nuevo

--- Front/Proyecto_Angular/appLibreria/src/app/book-catalog/component/list-book/list-book.component.ts
+++ Front/Proyecto_Angular/appLibreria/src/app/book-catalog/component/list-book/list-book.component.ts
@@ -43,7 +43,7 @@ export class ListBookComponent {
     this.cargarTablaLibros();
     this.authorsService.getAllAuthors().subscribe(
       (resp) => {
-        this.autores = resp;
+        this.autores = resp.content;
         //console.log(resp);
       },
       (error) => {
