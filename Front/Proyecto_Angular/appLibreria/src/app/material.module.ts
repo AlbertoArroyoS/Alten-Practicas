f0b5b39dc764b67829c217b213e86d5137262f76
Alberto Arroyo <albertoarroyo@hotmail.es>
2024-05-08 16:00:28 +0200

    pruebas

---
+++ Front/Proyecto_Angular/appLibreria/src/app/material.module.ts
@@ -0,0 +1,85 @@
+import { NgModule } from "@angular/core";
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatRippleModule } from '@angular/material/core';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatListModule } from '@angular/material/list';
+
+@NgModule({
+    imports: [
+      MatToolbarModule,
+      MatButtonModule,
+      MatFormFieldModule,
+      MatIconModule,
+      MatInputModule,
+      MatSelectModule,
+      MatCheckboxModule,
+      MatDialogModule,
+      MatTooltipModule,
+      MatTableModule,
+      MatCardModule,
+      MatRippleModule,
+      MatProgressBarModule,
+      MatPaginatorModule,
+      MatSortModule,
+      MatMenuModule,
+      MatDatepickerModule,
+      MatRadioModule,
+      MatAutocompleteModule,
+      MatProgressSpinnerModule,
+      MatGridListModule,
+      MatDividerModule,
+      MatNativeDateModule,
+      MatSnackBarModule,
+      MatListModule,
+    ],
+    exports: [
+      MatToolbarModule,
+      MatButtonModule,
+      MatFormFieldModule,
+      MatIconModule,
+      MatInputModule,
+      MatSelectModule,
+      MatCheckboxModule,
+      MatDialogModule,
+      MatTooltipModule,
+      MatTableModule,
+      MatCardModule,
+      MatRippleModule,
+      MatProgressBarModule,
+      MatPaginatorModule,
+      MatSortModule,
+      MatMenuModule,
+      MatDatepickerModule,
+      MatRadioModule,
+      MatAutocompleteModule,
+      MatProgressSpinnerModule,
+      MatGridListModule,
+      MatDividerModule,
+      MatNativeDateModule,
+      MatSnackBarModule,
+      MatListModule,
+    ],
+  })
+
+export class MaterialModule { }
