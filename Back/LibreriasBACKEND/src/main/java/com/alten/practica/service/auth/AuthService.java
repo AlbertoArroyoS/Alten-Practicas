e86d02ddddbd18f8ded1115644c505640baa0125
Alberto Arroyo <albertoarroyo@hotmail.es>
2024-05-15 11:09:06 +0200

    enum para los roles de usuario

--- Back/LibreriasBACKEND/src/main/java/com/alten/practica/service/auth/AuthService.java
+++ Back/LibreriasBACKEND/src/main/java/com/alten/practica/service/auth/AuthService.java
@@ -1,5 +1,25 @@
 package com.alten.practica.service.auth;
 
+import org.springframework.stereotype.Service;
+
+import com.alten.practica.modelo.entidad.dto.AuthDTO;
+import com.alten.practica.modelo.entidad.dto.request.LoginDTORequest;
+import com.alten.practica.modelo.entidad.dto.request.RegisterDTORequest;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class AuthService {
+	
+	public AuthDTO login(LoginDTORequest request) {
+		//return AuthDTO.builder().token("token").build();
+		return null;
+	}
+
+	public AuthDTO register(RegisterDTORequest request) {
+		//return AuthDTO.builder().token("token").build();
+		return null;
+	}
 
 }
---
+++ Back/LibreriasBACKEND/src/main/java/com/alten/practica/util/Role.java
@@ -0,0 +1,5 @@
+package com.alten.practica.util;
+
+public enum Role {
+	ADMIN, USER
+}
